a68b9055a0605ed17f32a9fae2973bd590b3cc92
d1runberg <derek.runberg@sparkfun.com>
2020-03-09 15:31:53 -0700

    Update to combo

--- gatorenvironment.ts
+++ gatorenvironment.ts
@@ -35,7 +35,7 @@ namespace gatorEnvironment {
 	*/
 	//% weight=32 
 	//% blockId="gatorEnvironment_beginEnvironment" 
-	//% block="initialize gator:Environment sensors"
+	//% block="initialize Qwiic Environmental Combo Board"
 	//% shim=gatorEnvironment::beginEnvironment
 	export function beginEnvironment(){
 		return
